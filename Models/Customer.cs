b0a3827a6aee8bddb350652d56ff676395053cd1
Ashraf Patel <4shr4f@live.co.uk>
2020-09-20 08:12:41 +0100

    Seeded Database Added Membership Types and Data Annotations

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -15,5 +15,8 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
